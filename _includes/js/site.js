87ecfdcb6db26a25b3aac567e5bc3dded33fd6a8
Schola Vesperis <scholavesperis@yahoo.fr>
2014-10-10 16:17:44 +0200

    Ouvrir les pdf's en local dans une nouvelle fenêtre.

--- _includes/js/site.js
+++ _includes/js/site.js
@@ -168,11 +168,33 @@
 			if (href.indexOf(server) >= 0) local = true;
 		}
 		if (local) {
-			e.preventDefault();
-			//$.scrollTo('#content', 800);
-			history.pushState(null, null, href);
-			navigate();
-			return false;
+			link = e.target;
+			if (href.split("?")[0].substr(-4).toLowerCase() == ".pdf") {
+				if (!link.hasAttribute("target")) {
+					link.setAttribute("target", "_blank");
+					
+					// Cancel action and regenerate a click - otherwise the target attribute isn't used
+					e.preventDefault();
+					if (document.createEvent) { // Firefox
+						var event = document.createEvent("MouseEvents");
+						event.initEvent("click", true, true);
+						link.dispatchEvent(event);
+					}	else if (link.click) { // IE
+						link.click();
+					} else {
+						console.log("no click method");
+					}
+					return false;
+				}
+			} else {
+				e.preventDefault();
+				//$.scrollTo('#content', 800);
+				history.pushState(null, null, href);
+				navigate();
+				return false;
+			}
+		} else {
+			// TODO check for dropbox links
 		}
 	};
 
