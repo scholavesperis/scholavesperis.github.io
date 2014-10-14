83c1602d8676b12fc8f295d003cd619c19a01677
Schola Vesperis <scholavesperis@yahoo.fr>
2014-10-14 19:32:28 +0200

    Google analytics

--- _includes/js/site.js
+++ _includes/js/site.js
@@ -1,3 +1,13 @@
+// Google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-55744005-1', 'auto');
+ga('send', 'pageview');
+
+// Articles list
 (function(){
 	var perPage = 10;
 	var menu = document.getElementById("menu");
@@ -148,7 +158,11 @@
 		//console.log(categ);
 		document.getElementById(categ).className = "active";
 		
-		if (noscroll !== true) menu.scrollIntoView(true);
+		if (noscroll !== true) {
+			menu.scrollIntoView(true);
+			// register page change with google analytics
+			ga('send', 'pageview');
+		}
 		
 		return true;
 	};
@@ -231,7 +245,7 @@
 
 })();
 
-// Audio
+// Audio widget
 (function(){
 	var player = document.getElementById("audio-player");
 	var progress = document.getElementById("audio-progress");
