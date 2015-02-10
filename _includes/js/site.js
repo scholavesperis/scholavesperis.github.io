c4788a28d49c39a998802f515f9fc73dac0c7561
jpeg729 <jpeg729@gmail.com>
2015-02-10 12:29:07 +0100

    Blocker les spams russes dans l'analyse du trafic

--- _includes/js/site.js
+++ _includes/js/site.js
@@ -5,7 +5,18 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-55744005-1', 'auto');
-ga('send', 'pageview');
+function gaPageview() {
+  if (window.location.pathname !== "/") {
+		ga('send', 'pageview');
+	} else {
+		console.log('homepage');
+		ga('send', 'pageview', {
+			'page': '/accueil',
+			'title': 'Schola Vesperis'
+		});
+	}
+}
+gaPageview();
 
 // Articles list
 (function(){
@@ -208,7 +219,7 @@ ga('send', 'pageview');
 		if (noscroll !== true) {
 			menu.scrollIntoView(true);
 			// register page change with google analytics
-			ga('send', 'pageview');
+			gaPageview();
 		}
 		
 		return true;
